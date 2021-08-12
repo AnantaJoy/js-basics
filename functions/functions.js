0a4b686c4eb635751e5c5e7eefccb722c2582260
Ananta <anantajoy007@gmail.com>
2021-08-12 19:10:33 +0600

    basics of javascript

--- functions/functions.js
+++ functions/functions.js
@@ -3,3 +3,14 @@ function addSum(num1, num2) {
 }
 
 addSum(5,7);
+addSum(10,12);
+
+// Returning something from a function
+
+function square(num) {
+    return num*num;
+}
+
+addOne = square(3) + 1
+console.log(addOne);
+
